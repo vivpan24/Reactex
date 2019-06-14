1531b3b8cbef8e48d24a32a8f9bcaafc372bc849
Vivek Pandey <vivek2407pandey@gmail.com>
2019-06-14 14:16:35 +0530

    first commit

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         My name is Vivek Kumar pandey
+
         </a>
       </header>
     </div>
